7c4381e112bb51b194e229893971c8f6cb4cee2e
AmanGuptAnalytics <98139823+AmanGuptAnalytics@users.noreply.github.com>
2023-03-14 08:34:52 +0530

    Update yahoo_pipeline.py

--- yahoo_pipeline.py
+++ yahoo_pipeline.py
@@ -3,21 +3,24 @@ import json
 from yahoofinancials import YahooFinancials
 
 @dlt.source
-def yahoofin(date='2022-09-15'):
-    return currency_exchange(date)
+def yahoofin(start_date='2022-01-01',end_date='2022-03-01'):
+    return currency_exchange(start_date, end_date )
 
 @dlt.resource(write_disposition="replace")
-def currency_exchange(date):
+def currency_exchange(start_date,end_date ):
     curr_pair=['USDINR=X']
+    print('This is the end date', end_date)
     yahoo_financials_currency = YahooFinancials(curr_pair)
-    daily_currency_prices = yahoo_financials_currency.get_historical_price_data(date, date, 'daily')
+    daily_currency_prices = yahoo_financials_currency.get_historical_price_data(start_date, end_date, 'daily')
+    
     yield daily_currency_prices
 
 
 if __name__=='__main__':
     # configure the pipeline with your destination details
-    pipeline = dlt.pipeline(pipeline_name='currency_exch', destination='bigquery', dataset_name='yahoo_fin2')
-
+    pipeline = dlt.pipeline(pipeline_name='currency_exch', destination='bigquery', dataset_name='yahoo_fin3')
     load_info = pipeline.run(yahoofin())
     # pretty print the information on data that was loaded
     print(load_info)
+    since_date = dlt.state().setdefault("end_date", {})
+    print('The date is as follows', since_date)
