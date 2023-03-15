f2e23309a6add3ed4ce408834b5b373346e23704
AmanGuptAnalytics <98139823+AmanGuptAnalytics@users.noreply.github.com>
2023-03-15 21:24:05 +0530

    Update yahoo_pipeline.py

--- yahoo_pipeline.py
+++ yahoo_pipeline.py
@@ -10,23 +10,21 @@ def yahoofin(start_date='2022-02-01',end_date='2022-03-01'):
 @dlt.resource(write_disposition="append")
 def currency_exchange(start_date,end_date ):
     curr_pair=['USDINR=X']
-    print('This is the end date', end_date)
-    dt = dlt.state().setdefault("date", '1970-01-01')
-    print('what we got in state or default ',dt)
-    dlt.state()["date"] = '2021-02-12'
-    print('what we set to state',dt)
-
+    print("this is the state", dlt.state().setdefault("last_loaded_date",[]))
+    s = dlt.state().setdefault("incremental_dict", {'last_loaded_date':'1970-01-01'})
+    print('what we got in state or default ',s)
+    s['last_loaded_date'] = '2021-01-01'
     yahoo_financials_currency = YahooFinancials(curr_pair)
     daily_currency_prices = yahoo_financials_currency.get_historical_price_data(start_date, end_date, 'daily')
     yield daily_currency_prices
+    print('what we set to state',s)
     
 
 if __name__=='__main__':
     # configure the pipeline with your destination details
 
-    pipeline = dlt.pipeline(pipeline_name='currency_exch', destination='bigquery', dataset_name='yahoo_fin3', full_refresh='True')
+    pipeline = dlt.pipeline(pipeline_name='currency_exch12', destination='bigquery', dataset_name='yahoo_fin317', full_refresh='False')
     
     load_info = pipeline.run(yahoofin())
     # pretty print the information on data that was loaded
     print(load_info)
-    
